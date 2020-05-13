f6b76ebe7bedacac5ab816855022eef0b82cfbbd
AhmetKaptan <ahmetoguzhana@gmail.com>
2020-05-13 17:57:58 +0300

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,11 @@ public class CreateChannel {
 
         System.out.println("slack project");
 
+        if (8>9){ //changed from intellij
+            System.out.println("false");
+        }else{
+            System.out.println("true");
+        }
+
     }
 }
